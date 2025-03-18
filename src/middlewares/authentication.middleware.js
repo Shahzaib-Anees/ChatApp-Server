59d50bcbf03e6bb94fff58d59875dc7a25a19da1
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2025-03-18 07:17:55 +0500

    Removing bug of access secret

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -1,11 +1,18 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 // authenticate user middleware
 const authenticate = async (req, res, next) => {
   const token = req.headers["authorization"] || req.headers["Authorization"];
+  console.log("token", token);
+
   if (!token) return res.status(404).json({ message: "no token found" });
 
   jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: "invalid token" });
+    if (err) {
+      console.log(err);
+      return res.status(403).json({ message: "invalid token" });
+    }
     req.user = user;
     next();
   });
